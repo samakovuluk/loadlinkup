c075c6fc2468d2cd18b8b95c1ca784378753fac3
Ulukbek <uluk.samakov@gmail.com>
2022-12-17 13:37:00 +0400

    changes

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -27,7 +27,7 @@ const SignIn = () => {
           <div className="flex flex-col space-y-4">
             <Auth
               supabaseClient={supabaseClient}
-              providers={['github']}
+              providers={['google']}
               redirectTo={getURL()}
               magicLink={true}
               appearance={{
