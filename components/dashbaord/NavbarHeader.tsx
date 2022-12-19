b135f7b690da59ab0632c87ad4f493485c56053e
Ulukbek <uluk.samakov@gmail.com>
2022-12-19 14:54:57 +0400

    dashboard table

--- components/dashbaord/NavbarHeader.tsx
+++ components/dashbaord/NavbarHeader.tsx
@@ -28,7 +28,7 @@ export default function NavbarHeader() {
                         </form>
                         </div>
                     <div className="flex items-center lg:order-2">
-                        <button type="button" className="hidden sm:inline-flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"><svg aria-hidden="true" className="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg> New Widget</button>
+                        <button type="button" className="hidden sm:inline-flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"><svg aria-hidden="true" className="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg> New Load</button>
                         <button id="toggleSidebarMobileSearch" type="button" className="p-2 text-gray-500 rounded-lg lg:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                             <span className="sr-only">Search</span>
                         
---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,81 @@
+
+
+
+
+const PrivacyPolicy = () => {
+    return (
+        <div className="flex justify-between px-6 mx-auto  bg-white dark:bg-gray-900">
+        <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+            <header className="mb-4 lg:mb-6 not-format">
+            <br></br>
+            <br></br>
+                <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-8 lg:text-4xl dark:text-white">Carrierify Privacy Policy</h1>
+            </header>
+            <p className="lead">Welcome to Carrierify, a platform that connects brokers with carriers to help them find the best shipping solutions. We are committed to protecting the privacy of our users and have developed this privacy policy to outline how we collect, use, and protect the personal information of our users. By using our service, you agree to the collection, use, and sharing of your personal information as described in this privacy policy.</p>
+            <br></br>
+            <p>Personal Information That We Collect:</p>
+            <br></br>
+            <p>Carrierify collects the following types of personal information from users:</p>
+            <br></br>
+            <p>* Name <br></br>
+                * Email address<br></br>
+                * Phone number<br></br>
+                * Information about the loads that brokers store on our platform</p><br></br>
+
+
+            <p>How We Use Personal Information:</p>
+            <br></br>
+
+
+            <p>We use the personal information that we collect from users to provide and improve our service, including:</p>
+            <br></br>
+
+            <p>* Communicating with users about their accounts and the service  <br></br>
+                * Responding to user inquiries and requests  <br></br>
+                * Analyzing and improving the service</p>  <br></br>
+
+           
+            <p>Sharing of Personal Information:</p>
+        
+            <p>
+            * We do not share the personal information that we collect with third parties, except in the following circumstances:
+            <br></br>
+* With the carrier who the broker has selected for a load <br></br>
+* If we are required to do so by law or to protect the rights, property, or safety of Carrierify, our users, or others <br></br><br></br>
+Protection of Personal Information: <br></br> 
+
+* We take reasonable measures to protect the personal information that we collect from users, including using secure servers and encrypting data. However, no method of transmission over the internet or method of electronic storage is 100% secure, and we cannot guarantee the absolute security of your personal information.
+<br></br> <br></br>
+Cookies and Tracking Technologies:
+<br></br>
+
+* We do not use cookies or other tracking technologies.
+<br></br>
+Access and Updates to Personal Information:
+<br></br>
+* Users can access and update their personal information by logging into their account on the Carrierify platform.
+<br></br><br></br>
+Handling User Concerns and Complaints:
+<br></br>
+* If you have any concerns or complaints about your privacy, please contact us using the contact information provided below.
+<br></br><br></br>
+Changes to the Privacy Policy:
+<br></br>
+
+* We reserve the right to update this privacy policy at any time. We will notify users of any changes by posting the updated policy on this page. We encourage users to review the privacy policy periodically to stay informed about how we are protecting the personal information we collect.
+<br></br><br></br>
+Contact Information:
+<br></br>
+
+* if you have any questions or concerns about this privacy policy or our handling of your personal information, please contact us at uluk.samakov@gmail.com.
+
+           </p>
+           <br></br>
+        </article>
+    </div>
+
+    )
+
+}
+
+export default PrivacyPolicy;
